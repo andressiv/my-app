b6d9989f768793e1f3123cac140d529c06a39d6e
andressiv <andressiv16@gmail.com>
2021-05-06 20:45:01 -0500

    added README.md

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import Nav from './Nav'
+
+
+function Navbar() {
+    return (
+      <div className="Navbar">
+        <Link to="#">Home</Link>
+        <Link to="#">Contacto</Link>
+      </div>
+    )
+}
+
+export default Navbar
+
+
